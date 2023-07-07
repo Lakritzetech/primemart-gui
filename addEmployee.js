4ef79fb8339c7bc2a0c98bb6f71b793d45c7ddab
Ogaitw <tiagoaugusto2402@gmail.com>
2023-07-07 20:28:00 -0300

    Implementado Mascara no CPF

--- addEmployee.js
+++ addEmployee.js
@@ -1,30 +1,41 @@
 function addEmployee() {
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const cpf = document.querySelector('#cpf').value;
-    const shift = document.querySelector('#shift').value;
-    const phone = document.querySelector('#phone').value;
-  
-    const employeeData = {
-      name,
-      email,
-      cpf,
-      shift,
-      phone
-    };
-  
-    axios.post('http://localhost:8080/employee', employeeData)
-      .then(response => {
-        console.log('Funcionário adicionado com sucesso:', response.data);
-        document.querySelector('#addEmployeeForm').reset();
-      })
-      .catch(error => {
-        const errorMessage = error.response.data.message;
-        document.getElementById('cpfError').textContent = errorMessage;
-      });
+  const cpf = document.querySelector('#cpf').value.replace(/\D/g, '');
+
+  if (cpf.length !== 11) {
+    document.getElementById('cpfError').textContent = 'CPF deve ter 11 dígitos';
+    return;
   }
-  
-  function goToIndex() {
-    window.location.href = 'index.html';
-  }
-  
+
+  const employeeData = {
+    name: document.querySelector('#name').value,
+    email: document.querySelector('#email').value,
+    cpf,
+    shift: document.querySelector('#shift').value,
+    phone: document.querySelector('#phone').value
+  };
+
+  axios.post('http://localhost:8080/employee', employeeData)
+    .then(response => {
+      console.log('Funcionário adicionado com sucesso:', response.data);
+      document.querySelector('#addEmployeeForm').reset();
+    })
+    .catch(error => {
+      const errorMessage = error.response.data.message;
+      document.getElementById('cpfError').textContent = errorMessage;
+    });
+}
+
+function goToIndex() {
+  window.location.href = 'index.html';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const cpfInput = document.querySelector('#cpf');
+  cpfInput.placeholder = '123.456.789-00';
+
+  cpfInput.addEventListener('input', function(event) {
+    const value = event.target.value.replace(/\D/g, '').slice(0, 11);
+    const formattedValue = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    cpfInput.value = formattedValue;
+  });
+});
